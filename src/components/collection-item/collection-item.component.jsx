7f4e77e9724b7fa3ab954d7dfc1dfc5c1a76717c
n13b9 <neeraj.barla92@gmail.com>
2022-03-10 10:37:10 +0530

    updating

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './collection-item.styles.scss'
+
+const CollectionItem =({id,name,imageUrl,price})=>(
+	<div className='collection-item'>
+		<div
+		className='image'
+		style={{
+			backgroundImage:`url(${imageUrl})`
+		}}
+		/>
+			<div className='collection-footer'>
+
+				<span className='name'> Name </span>
+				<span className='price'> Price </span>
+
+			</div>
+
+	</div>
+
+	);
+
+
+export default CollectionItem;
+
