612e1b7fe27b4ed558afdaf44510f8271d610608
n13b9 <neeraj.barla92@gmail.com>
2022-04-13 23:46:13 +0530

    shop selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -9,11 +9,16 @@ import {selectCollection} from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 
-const CollectionPage =(match)=>{
+const CollectionPage =({collection})=>{
+	const {title,items}=collection;
 	return (
 	<div className='category'>
-		<h2> colelction Page </h2>
-
+		<h2 className='title'> {title} </h2>
+		<div className='items'>
+			{
+				items.map(item=> <CollectionItem key={item.id} item={item} />)
+			}
+		</div>
 	</div>
 	)};
 
